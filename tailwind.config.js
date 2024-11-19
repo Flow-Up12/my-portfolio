f72b4b5b2bfef6ad3159e26c71fc6af4dbbfc073
Flow-Up12 <marcosje2005@icloud.com>
2024-11-18 21:07:36 -0600

    push

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#121212", // Black
+        secondary: "#f5f5f5", // White
+        accent: "#a9a9a9", // Gray
+      },
+    },
+  },
+  plugins: [],
+};
